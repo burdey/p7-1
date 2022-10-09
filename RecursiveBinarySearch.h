1b48e42b1842f2258ced3f7f7ad72471b9e39e56
burdey <88636823+burdey@users.noreply.github.com>
2022-10-09 23:35:40 +0800

    Add files via upload

---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,17 @@
+//
+// Created by Satori on 2022/10/9.
+//
+
+#ifndef P7_RECURSIVEBINARYSEARCH_H
+#define P7_RECURSIVEBINARYSEARCH_H
+#include <vector>
+
+class RecursiveBinarySearch {
+public:
+    RecursiveBinarySearch();
+    bool Search(std::vector<int> storage,int intent);
+    int binarySearch(std::vector<int> storage,int intent,int begin,int end);
+    ~RecursiveBinarySearch();
+};
+
+#endif //P7_RECURSIVEBINARYSEARCH_H
